d072a547b681e7baffb0180d5d34281617e3bd97
Denis Bittencourt <denis765@gmail.com>
2021-06-15 22:31:02 -0300

    Initial commit

---
+++ SiagriMicroService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SiagriMicroService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
