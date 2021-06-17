90b051c0fa89f84513b66742814793ee847272f7
Denis Bittencourt <denis765@gmail.com>
2021-06-17 08:03:49 -0300

    Project almost finished. Implemented services for calling the openweatherapi and spotifyapi.

---
+++ SiagriMicroService/Models/PlaylistResponse.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SiagriMicroService.Models
+{
+    public class PlaylistResponse
+    {
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("latitude")]
+        public double Latitude { get; set; }
+
+        [JsonProperty("longitude")]
+        public double Longitude { get; set; }
+
+        [JsonProperty("temperature")]
+        public double Temperature { get; set; }
+
+        [JsonProperty("playlist")]
+        public Playlist Playlist { get; set; }
+    }
+
+    public class Playlist
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("songs")]
+        public List<Song> Songs { get; set; }
+    }
+
+    public class Song
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("singers")]
+        public List<Singer> Singers { get; set; }
+    }
+
+    public class Singer
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
---
+++ SiagriMicroService/Services/WeatherService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SiagriMicroService.Models;
+
+namespace SiagriMicroService.Services
+{
+    public class WeatherService
+    {
+        private static string BASE_URL = "https://api.openweathermap.org/data/2.5/weather?";
+        private static string PARAM_LATITUDE = "lat=";
+        private static string PARAM_LONGITUDE = "lon=";
+        private static string PARAM_UNITS = "units=metric";
+        private static string PARAM_APPID = "appid=0eaa098ae833fae2862d8a2244f8e8a3";
+
+        public static async Task<OpenWeatherMapReponse> GetWeatherAsync(double lat, double lon)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var url = BASE_URL + PARAM_LATITUDE + lat + "&" + PARAM_LONGITUDE + lon + "&" + PARAM_UNITS + "&" + PARAM_APPID;
+
+                    var response = await httpClient.GetAsync(url);
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var temp = JsonConvert.DeserializeObject<OpenWeatherMapReponse>(responseString);
+
+                        return temp;
+                    }
+
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+
+                //TODO: LOG THE EXCEPTIONS
+            }
+        }
+    }
+}
